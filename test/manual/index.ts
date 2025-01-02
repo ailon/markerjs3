8ed9836dcbfcc9555abddc80e53a7ca503f5c3ad
Alan Mendelevich <ailon@ailon.org>
2025-01-02 15:06:52 +0200

    add `CustomImageMarker` as a representation of a user-set image marker

--- test/manual/index.ts
+++ test/manual/index.ts
@@ -3,6 +3,7 @@ import { Renderer } from '../../src/Renderer';
 import {
   AnnotationState,
   ArrowType,
+  CustomImageMarker,
   ShapeOutlineMarkerBaseState,
 } from '../../src/core';
 import {
@@ -187,7 +188,13 @@ export class Experiments {
 
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   public addMarker(markerType: string) {
-    this.markerArea1?.createMarker(markerType);
+    const editor = this.markerArea1?.createMarker(markerType);
+
+    // for custom image marker set a user-defined image
+    if (editor && editor.marker instanceof CustomImageMarker) {
+      editor.marker.svgString = `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><g fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"><rect width="18" height="18" x="3" y="3" rx="2" ry="2"/><circle cx="9" cy="9" r="2"/><path d="m21 15l-3.086-3.086a2 2 0 0 0-2.828 0L6 21"/></g></svg>`;
+    }
+
     // const newMarkerEditor = this.markerArea1?.createMarker(markerType);
     // if (
     //   newMarkerEditor &&
